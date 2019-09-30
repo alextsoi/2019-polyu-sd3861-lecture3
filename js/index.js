93239955e50a4c6059381294d41604dabb6f431e
Alex Tsoi <alex@k2.digital>
2019-09-30 10:58:20 +0800

    updated the self checking tool

---
+++ js/index.js
@@ -0,0 +1,134 @@
+$(document).ready(function(){
+    var ajaxLock = false;
+    var taskCompleted = 0;
+    var desiredNumberOfTasks = 3;
+
+    var task1LinkPrefix = 'https://raw.githubusercontent.com/';
+    var task1LinkSuffix = '/2019-polyu-sd3861-lecture2/master/README.md';
+
+    var task2LinkPrefix = 'https://raw.githubusercontent.com/';
+    var task2LinkSuffix = '/2019-polyu-sd3861-lecture2/master/lecture2.md';
+
+    var task3LinkPrefix = 'https://raw.githubusercontent.com/';
+    var task3LinkSuffix = '/2019-polyu-sd3861-lecture2/master/docs/index.html';
+
+
+    function taskCompletedCheck(){
+        if(desiredNumberOfTasks === taskCompleted){
+            ajaxLock = false;
+            taskCompleted = 0;
+            $('#submit').prop('disabled', false);
+        }else{
+            console.log('Total: ' + desiredNumberOfTasks);
+            console.log('Completed: ' + taskCompleted);
+        }
+    }
+
+    $('#form').on('submit', function(e){
+        if(ajaxLock){
+            alert('We are asking GitHub for the results, please wait.');
+            return;
+        }
+        e.preventDefault();
+        var githubAccount = $.trim($('#github_account').val());
+        if(githubAccount === ''){
+            alert('Please check your github account name');
+            return;
+        }
+
+        ajaxLock = true;
+        $('#submit').prop('disabled', true);
+
+
+        $('#result1').text('');
+        $('#result1_status').html('');
+        $('#result1_detail').html('');
+        $('#result2').text('');
+        $('#result1_status').html('');
+        $('#result2_detail').html('');
+        $('#result3').text('');
+        $('#result1_status').html('');
+        $('#result3_detail').html('');
+
+        var task1Url = task1LinkPrefix + githubAccount + task1LinkSuffix;
+        // Task 1
+        $.ajax({
+            method: 'GET',
+            url: task1Url,
+            success: function(data){
+                var textString = $.trim(data);
+                textString = textString.toLowerCase();
+
+                taskCompleted++;
+                taskCompletedCheck();
+                $('#result1').text(data);
+                if(textString === 'lecture 2'){
+                    $('#result1_status').html('<span class="badge badge-success">Success</span>');
+                }else{
+                    $('#result1_status').html('<span class="badge badge-danger">Oops</span>');
+                }
+                $('#result1_detail').html('<a href="' + task1Url + '" target="_blank" class="badge badge-info">Link</a>');
+            },
+            error: function(){
+                taskCompleted++;
+                taskCompletedCheck();
+                $('#result1').text('');
+                $('#result1_status').html('<span class="badge badge-danger">Oops</span>');
+                $('#result1_detail').html('<a href="' + task1Url + '" target="_blank" class="badge badge-info">Link</a>');
+
+            }
+        });
+
+        var task2Url = task2LinkPrefix + githubAccount + task2LinkSuffix;
+        // Task 2
+        $.ajax({
+            method: 'GET',
+            url: task2Url,
+            success: function(data){
+                var textString = $.trim(data);
+                textString = textString.toLowerCase();
+
+                taskCompleted++;
+                taskCompletedCheck();
+                $('#result2').text(data);
+                $('#result2_status').html('<span class="badge badge-success">Success</span>');
+                $('#result2_detail').html('<a href="' + task2Url + '" target="_blank" class="badge badge-info">Link</a>');
+            },
+            error: function(response){
+                if(response.status == 404){
+                    $('#result2').text('Note: lecture2.md is not found.');
+                }
+                taskCompleted++;
+                taskCompletedCheck();
+                $('#result2_status').html('<span class="badge badge-danger">Oops</span>');
+                $('#result2_detail').html('<a href="' + task2Url + '" target="_blank" class="badge badge-info">Link</a>');
+            }
+        });
+
+        var task3Url = task3LinkPrefix + githubAccount + task3LinkSuffix;
+        // Task 2
+        $.ajax({
+            method: 'GET',
+            url: task3Url,
+            success: function(data){
+                var textString = $.trim(data);
+                textString = textString.toLowerCase();
+
+                taskCompleted++;
+                taskCompletedCheck();
+                $('#result3').text(data);
+                $('#result3_status').html('<span class="badge badge-success">Success</span>');
+                $('#result3_detail').html('<a href="' + task3Url + '" target="_blank" class="badge badge-info">Link</a>');
+            },
+            error: function(response){
+                if(response.status == 404){
+                    $('#result3').text('Note: lecture2.md is not found.');
+                }
+                taskCompleted++;
+                taskCompletedCheck();
+                $('#result3_status').html('<span class="badge badge-danger">Oops</span>');
+                $('#result3_detail').html('<a href="' + task3Url + '" target="_blank" class="badge badge-info">Link</a>');
+            }
+        });
+    });
+});
